a1fb0fe728c16cf20d897f0a188ca606fed6e52b
Michelle S Lee <michlee1337@gmail.com>
2018-09-21 20:56:31 +0900

    data

---
+++ main.py
@@ -0,0 +1,13 @@
+# predicting sum of customer transactions from Google Merchandise Store data
+
+# Machine Learning: Linear Regression
+
+# importing libraries
+import numpy as np
+import pandas as pd
+from sklearn import preprocessing
+data = pd.read_csv("train.csv.zip")
+
+# feature normalization
+
+print("Loading... ")
