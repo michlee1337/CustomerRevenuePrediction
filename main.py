b90e358cd0fdde27fdb41a96dd97992482180df0
Michelle S Lee <michlee1337@gmail.com>
2018-10-11 03:48:57 +0900

    changed data import code to support JSON columns

--- main.py
+++ main.py
@@ -1,33 +1,33 @@
-
 # import libraries
-import scipy
-import numpy
-import matplotlib
+
 import pandas as pd
-import sklearn
-import zipfile
-import datetime
+import numpy as np
+import matplotlib.pyplot as plt
+#import seaborn as sns
+import json
+from pandas.io.json import json_normalize
+from datetime import datetime
+from sklearn import preprocessing
 
-# import data
-zf = zipfile.ZipFile('train.csv.zip')
-df = pd.read_csv(zf.open('train.csv'))
+# load data and convert columns w JSON data into normal columns
+def load_df(csv_path, nrows=None):
+    JSON_COLUMNS = ['device', 'geoNetwork', 'totals', 'trafficSource']
 
-# setting column dtypes
-df['channelGrouping'].astype('category')
-df['date'] = pd.to_datetime(df['date'], format='%Y%m%d')
-df['device'] = df['device'].astype('object')
-df['fullVisitorId'] = df['fullVisitorId'].astype('str')
-df['geoNetwork'] = df['geoNetwork'].astype('object')
-df['sessionId'] = df['sessionId'].astype('object')
-df['socialEngagementType'] = df['socialEngagementType'].astype('category')
-df['totals'] = df['totals'].astype('object')
-df['trafficSource'] = df['trafficSource'].astype('object')
-df['visitId'] = df['visitId'].astype('uint64')
-df['visitStartTime'] = df['visitStartTime'].astype('uint64')
+    df = pd.read_csv(csv_path,
+                     converters={column: json.loads for column in JSON_COLUMNS},
+                     dtype={'fullVisitorId': 'str'}, # Important!!
+                     nrows=nrows)
 
-import ast
-for i in range(df.shape[0]):
-    df.at["rev",i] = ast.literal_eval(df.loc[i,'totals']).get("transactionRevenue")
+    for column in JSON_COLUMNS:
+        column_as_df = json_normalize(df[column])
+        column_as_df.columns = [f"{column}.{subcolumn}" for subcolumn in column_as_df.columns]
+        df = df.drop(column, axis=1).merge(column_as_df, right_index=True, left_index=True)
+    print(f"Loaded. Shape: {df.shape}")
+    return df
 
-# looking at data
-print(df.shape)
+# main code
+data = load_df('train.csv')
+
+print('Loaded :)')
+print(data.shape())
+print(data.iloc[0])
